1635da34f6c32230fa0d7f99553d4c2be0722efd
GutoQuirino <tavinho_vh@hotmail.com>
2022-06-09 18:01:26 -0300

    alter

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 function geraCodigo(){
     let d = new Date();
     console.log(d);
+    let renova = document.querySelector('#codrenova');
+    let emergency = document.getElementById('codemergencia');
+    
 }
