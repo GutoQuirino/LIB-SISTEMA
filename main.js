5c0617dd3c59b993915b0290ef74108f6bf53f20
GutoQuirino <tavinho_vh@hotmail.com>
2022-06-09 19:26:41 -0300

    dgjhg

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function geraCodigo(){
-    let d = new Date();
-    console.log(d);
+    let d = new Date(1900,00,01);
+    console.log(d.toISOString())
     let renova = document.querySelector('#codrenova').innerHTML= 'numero de serie p/ 30 dias';
     let emergency = document.getElementById('codemergencia').innerHTML= ' numero de serie p/ 3 dias';
     
