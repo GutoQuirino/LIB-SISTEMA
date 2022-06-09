1f8ff4fcad9f8a10bc7ea155ca3971f6675cd7fa
GutoQuirino <tavinho_vh@hotmail.com>
2022-06-09 18:36:05 -0300

    add funçao

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function geraCodigo(){
     let d = new Date();
     console.log(d);
-    let renova = document.querySelector('#codrenova');
-    let emergency = document.getElementById('codemergencia');
+    let renova = document.querySelector('#codrenova').innerHTML= 'numero de serie p/ 30 dias';
+    let emergency = document.getElementById('codemergencia').innerHTML= ' numero de serie p/ 3 dias';
     
 }
