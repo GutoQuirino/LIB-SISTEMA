81ee667a319db7d6658ae395cb0d327faee54a1f
GutoQuirino <tavinho_vh@hotmail.com>
2022-06-10 16:43:28 -0300

    fgjfg

--- main.js
+++ main.js
@@ -6,10 +6,8 @@ function geraCodigo(){
     let somaAno = (ano - 1900)*365;
     let bisexto = (ano - 1900)/4;
     let somaMes = (mes)*30; // verificar se é 0 ou 1
-    let res = (somaAno + somaMes + dia + bisexto)+ 2;
-    let chCadastro = 0+ document.querySelector('#ncadastro');
-    let formula = '000'+"cod2"+chCadastro[3]+"cod1"+chCadastro[6]+'000';
-    let renova = document.querySelector('#codrenova').innerHTML= formula;
-    let emergency = document.getElementById('codemergencia').innerHTML= ' numero de serie p/ 3 dias';
-    
+    let result = number(somaAno + somaMes + dia + bisexto)+ 2;
+    let chCadastro = document.querySelector('#ncadastro').value;
+    let inicio = chCadastro.substr(0,2);
+    let final = chCadastro.substr(2,3);
 }
