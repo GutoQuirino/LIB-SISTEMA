a406ee2008578de152f4ba25b9fa1cf17ff9da50
GutoQuirino <tavinho_vh@hotmail.com>
2022-06-10 10:36:12 -0300

    cbh

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ function geraCodigo(){
     let bisexto = (ano - 1900)/4;
     let somaMes = (mes)*30; // verificar se é 0 ou 1
     let res = (somaAno + somaMes + dia + bisexto)+ 2;
-    let renova = document.querySelector('#codrenova').innerHTML= res;
+    let chCadastro = 0+ document.querySelector('#ncadastro');
+    let formula = '000'+"cod2"+chCadastro[3]+"cod1"+chCadastro[6]+'000';
+    let renova = document.querySelector('#codrenova').innerHTML= formula;
     let emergency = document.getElementById('codemergencia').innerHTML= ' numero de serie p/ 3 dias';
     
 }
